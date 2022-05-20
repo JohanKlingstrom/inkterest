82a4eff606dea46c3a67b963be56ccd2bd7ac81b
johk <johkli19@hotmail.se>
2022-05-20 12:13:41 +0200

    added spinner loading on feed

--- inkterest_frontend/src/components/Feed.jsx
+++ inkterest_frontend/src/components/Feed.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { client } from '../client';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
 
 const Feed = () => {
+  const [loading, setLoading] = useState(false)
+
+  if(loading) return <Spinner message="We are adding new designs to your feed!" />
   return (
-    <div>Feed</div>
+    <div>
+      
+    </div>
   )
 }
 
